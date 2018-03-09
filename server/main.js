eeaa4f7cc7b7055eb410c368a39832b5cb1ee032
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2018-03-08 21:10:14 -0500

    Added skeleton for the Node.js service

---
+++ server/main.js
@@ -0,0 +1,38 @@
+/** 
+ * DEPENDENCIES 
+ */
+
+const Express = require("express");
+
+/**
+ * CONSTANTS
+ */
+
+const express = Express();
+const router = Express.Router();
+const port = 8023;
+
+/**
+ * HANDLERS & EXECUTION
+ */
+
+// route prefix
+express.use("/sitecore-dxg", router);
+
+// url: <domain>:8023/sitecore-dxg/status
+router.get("/status", (request, response) => {
+    response.json({ message: "Online" });
+});
+
+// url: <domain>:8023/sitecore-dxg/generate/mdj
+router.get("/generate/mdj", (request, response) => {
+    response.json({ message: "Metadata-json file generation has not yet been implemented" });
+});
+
+// url: <domain>:8023/sitecore-dxg/generate/mdj
+router.get("/generate/documentation", (request, response) => {
+    response.json({ message: "HTML Documentation generation has not yet implemented" });
+});
+
+// start listening
+express.listen(port, () => console.log(`Sitecore DXG Service Started. Listening on port ${port}`));
