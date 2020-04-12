fa8403e026a664a9b2f6d30f3e1c00fb0785c580
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2020-04-12 17:29:06 -0400

    WIP Adds support for pretty-printing JSON output (using while debugging and may keep), and fixes issue with project layer configuration referencing Foundation modules (generating successfully)

--- middlemen/JobQueue/jobqueue_writer.js
+++ middlemen/JobQueue/jobqueue_writer.js
@@ -68,6 +68,8 @@ if (!queue) {
     return;
 }
 
+var pretty = args.length > 3 ? args[3] : false;
+
 // TODO: change this function to get the generation source described by user inputs instead of static test data
 var getGenerationSource = () => {
     const masterDatabaseName = "master";
@@ -99,7 +101,7 @@ var getGenerationSource = () => {
         new Documentation_Types.HelixLayerMap(
             "825B30B4-B40B-422E-9920-23A1B6BDA89C".toLowerCase(),
             Documentation_Types.HelixLayerNames.Project,
-            "393E4EE8-4160-4467-8D42-371FF6A38354|4B333E08-BBB7-48AD-8150-8203BE0CB77C|ADCF25DC-5993-4C20-8204-83DE5A35CBCB|E422EBC8-55CA-40A7-A66A-EB592084516A|7681CF47-9D26-476F-A395-42C0C8D4F18F|7C610278-908F-487C-B32E-C81696D43396|DF639DE8-C9E7-4B18-967E-B1A14A8FF221|53A22396-4581-459D-80D6-47135E1F6F3B|5FD62CDC-BD6E-408C-B88F-9D9C41462CD8|16E64A6E-34A7-42F3-8678-010CC5E4FB39|197A3308-E54C-4E72-944C-A4885C6297E3"
+            "104E6124-706D-4620-9DB6-F8814BE5F471|6C3B2801-5573-40CF-87AD-A2CB94FCE40E|3DA20332-8D97-4B8C-BFE4-E482E68854EE"
                 .toLowerCase()
                 .split("|")));
 
@@ -144,7 +146,12 @@ amqp.connect(connectionString, function(err, connection) {
 
             channel.consume(q.queue, function(msg) {
                 if (msg.properties.correlationId === generationId) {
-                    console.log(' [.] Generation Response: %s', msg.content.toString());
+                    var rawReply = msg.content.toString();
+                    var output = pretty
+                        ? JSON.stringify(JSON.parse(rawReply), null, 4)
+                        : rawReply;
+                    
+                    console.log(` [.] Generation Response: \n \n ${output}\n \n`);
                     setTimeout(function() {
                         setTimeout(function() { _exitProgram(process, 0, connection) }, 500);
                     }, 500);
